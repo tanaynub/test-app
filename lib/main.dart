e47a234fd67d92b211a44c4a3fe72e07e69287a4
Tandy ! <ustanay4@gmail.com>
2020-08-11 11:42:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        color: Colors.blueGrey,
+        home: Scaffold(
+          // backgroundColor:
+          appBar: AppBar(
+            title: Text('Calculator'),
+            backgroundColor: Colors.black,
+          ),
+        ));
+  }
+}
